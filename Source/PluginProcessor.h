164717eded18727ce2585e4504799306b64d0569
Mordorkonan <konanmordormordor@gmail.com>
2023-07-02 14:28:15 +0300

    minor fix with Fifo size

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -69,7 +69,7 @@ public:
     void getStateInformation (juce::MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
-    Fifo<float> fifo;
+    Fifo<float, 256> fifo;
 
 private:
     //==============================================================================
